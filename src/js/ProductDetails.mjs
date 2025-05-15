e3e3f72242911d41f680f3ad635a267e23b10eb5
lantoniomxnusd <luisoteacher@gmail.com>
2025-05-15 16:20:59 -0600

    Team Assignment 2

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,41 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+    constructor(productId, dataSource){
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails();
+        document.getElementById('addToCart').addEventListener('click', this.addProductToCart.bind(this));         
+        
+    }
+    addProductToCart() {
+        const cartItems = getLocalStorage("so-cart") || []; 
+        // if (!Array.isArray(cartItems)) {
+        //     cartItems = [];
+        // }
+        cartItems.push(this.product);
+        setLocalStorage("so-cart", cartItems);
+        
+    }
+
+    renderProductDetails() {
+        buildProductDetails(this.product);
+    }
+}
+
+function buildProductDetails(product) {
+    document.querySelector('h3').textContent = product.Brand.Name;
+    document.querySelector('h2').textContent = product.NameWithoutBrand;
+
+    const image = document.getElementById('productImage');
+    image.src = product.Image;
+    image.alt = product.NameWithoutBrand;
+    document.getElementById('productPrice').textContent = product.FinalPrice;
+    document.getElementById('productColor').textContent = product.Colors.ColorName;
+    document.getElementById('productDescription').innerHTML = product.DescriptionHtmlSimple;
+    document.getElementById('addToCart').dataset.id = product.Id;
+}
