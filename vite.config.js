463ecee4c13580fcf512d0d978ec0ae8c9b8689e
elielino360 <appschoolbic@gmail.com>
2025-05-25 00:22:44 +0100

    week 3 assignment

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ build: {
       cart: resolve(__dirname, "src/cart/index.html"),
       checkout: resolve(__dirname, "src/checkout/index.html"),
       product: resolve(__dirname, "src/product_pages/index.html"),
-      productList: resolve(__dirname, "src/product_listing/index.html")
+      productList: resolve(__dirname, "src/product_listing/index.html"),
     },
   },
 },
