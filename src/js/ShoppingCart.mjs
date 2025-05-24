e539f70deb036af7550dd0a36554f027db7e9371
tuckervlittle <tucker.little95@gmail.com>
2025-05-23 15:12:14 -1000

    Added cart total and styling

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,12 +1,28 @@
 import { getLocalStorage } from './utils.mjs';
 
+let cartTotal = 0;
 export function renderCartContents() {
     let cartItems = getLocalStorage('so-cart');
     if (!Array.isArray(cartItems)) {
         cartItems = [];
     }
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector('.product-list').innerHTML = htmlItems.join('');
+    const cartList = document.querySelector('.cart-list')
+    cartList.innerHTML = htmlItems.join('');
+
+    console.log(cartTotal);
+    const total = document.querySelector('.cart-total');
+    const checkout = document.getElementById('checkout')
+    total.innerHTML = `<b>Total: $${cartTotal.toFixed(2)}</b>`;
+    if (cartTotal > 0) {
+        total.parentElement.classList.remove('hide');
+        checkout.classList.remove('hide');
+    }
+    else {
+        cartList.innerHTML = `
+        <p>It looks like your cart is empty...</p>
+        <p>Come back when you've decided on some gear!</p>`
+    }
 }
 
 function cartItemTemplate(item) {
@@ -31,6 +47,7 @@ function cartItemTemplate(item) {
     <p class="cart-card__quantity">qty: 1</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
     </li>`;
+    cartTotal += item.FinalPrice;
     
     return newItem;
 }
