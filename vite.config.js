658ff62c660a1db64ee52e9c3cc390c1a196f5af
elielino360 <appschoolbic@gmail.com>
2025-05-25 00:05:00 +0100

    Refactor

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,8 @@ build: {
       main: resolve(__dirname, "src/index.html"),
       cart: resolve(__dirname, "src/cart/index.html"),
       checkout: resolve(__dirname, "src/checkout/index.html"),
-      product: resolve(__dirname, "src/product_pages/index.html"
-      ),
-
+      product: resolve(__dirname, "src/product_pages/index.html"),
+      productList: resolve(__dirname, "src/product_listing/index.html")
     },
   },
 },
